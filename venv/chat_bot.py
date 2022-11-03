11dd09a5cff78458282380d0e43602a9c4401d40
kate <01redcat@mail.ru>
2022-11-03 13:40:35 +0300

    1

--- venv/chat_bot.py
+++ venv/chat_bot.py
@@ -9,9 +9,8 @@ def send_welcome(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message,'1 хочу продать usd за рубли\n 2 хочу купить usd за рубли')
+    if message == '1':
+        bot.reply_to('курс на сегодня 1 usd = 60 rub')
 
-@bot.message_handler(message=[1])
-def echo_all(message):
-	bot.reply_to(message, "курс на сегодня 1 usd = 60 rub")
 
 bot.infinity_polling()
