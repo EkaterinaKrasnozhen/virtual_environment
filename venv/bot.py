73033fb49ac3e82b67c55de49573704ec6631623
kate <01redcat@mail.ru>
2022-11-03 16:19:04 +0300

    bot py

--- venv/bot.py
+++ venv/bot.py
@@ -6,13 +6,6 @@ def get_text_messages(message):
        bot.send_message(message.from_user.id,'Привет, хотите совершить выгодный обмен?\n 1 - Купить usd за rub\n 2 - Продать usd за rub')
        
 
-get_text_messages(message):
-          if mes.text == '1':
-            bot.send_message(mes.from_user.id,'Курс на сегодняшний день 1 USD = 62 RUB')
-          elif mes.text == '2':
-            bot.send_message(mes.from_user.id, 'Курс на сегодняшний день 1 USD = 60 RUB')
-          else:
-            bot.send_message(mes.from_user.id, 'Я тебя не понимаю')
 
 
         
