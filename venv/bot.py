c3292227c25fcd4094cd22bb6bcea2ff788f4074
kate <01redcat@mail.ru>
2022-11-03 11:08:29 +0300

    bot to do

--- venv/bot.py
+++ venv/bot.py
@@ -2,7 +2,7 @@ import telebot
 bot = telebot.TeleBot('5483567139:AAFOLwOJcEr_oN7xhAz82MybfR-w4RIJ--A')
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    if message.text == 'Привет':
+    if message.text == 'привет':
         bot.send_message(message.from_user.id,'Привет, чем я могу помочь?')
     elif message.text == '/help':
         bot.send_message(message.from_user.id, 'Напиши привет')
