f0abb41ddafcb9346462eedcfa71f777cea5436f
kate <01redcat@mail.ru>
2022-11-03 13:17:10 +0300

    bot2

--- venv/bot.py
+++ venv/bot.py
@@ -4,14 +4,16 @@ bot = telebot.TeleBot('5483567139:AAFOLwOJcEr_oN7xhAz82MybfR-w4RIJ--A')
 def get_text_messages(message):
     if message.text == '/start':
        bot.send_message(message.from_user.id,'Привет, хотите совершить выгодный обмен?\n 1 - Купить usd за rub\n 2 - Продать usd за rub')
-       get_new_mes(message)
+       return
 
-def get_new_mes(mes):
-    if mes.text == '1':
-           bot.send_message(mes.from_user.id,'Курс на сегодняшний день 1 USD = 62 RUB')
-    elif mes.text == '2':
-          bot.send_message(mes.from_user.id, 'Курс на сегодняшний день 1 USD = 60 RUB')
-    else:
-           bot.send_message(mes.from_user.id, 'Я тебя не понимаю')
+get_text_messages(message):
+          if mes.text == '1':
+            bot.send_message(mes.from_user.id,'Курс на сегодняшний день 1 USD = 62 RUB')
+          elif mes.text == '2':
+            bot.send_message(mes.from_user.id, 'Курс на сегодняшний день 1 USD = 60 RUB')
+          else:
+            bot.send_message(mes.from_user.id, 'Я тебя не понимаю')
+
+
         
 bot.polling(none_stop=True, interval=0)
---
+++ venv/chat_bot.py
@@ -0,0 +1,13 @@
+import telebot
+
+bot = telebot.TeleBot("5612465487:AAEOLElRIYrar9GXEeEgp5zDk1huUNPb0Bw")
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Привет, хотите совершить выгодный обмен?")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.infinity_polling()
