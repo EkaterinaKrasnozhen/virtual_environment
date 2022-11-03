9102f72fc4cc9adffedf04f01bdb532879a57d3e
kate <01redcat@mail.ru>
2022-11-03 14:13:59 +0300

    bot 2

--- venv/tm_bot.py
+++ venv/tm_bot.py
@@ -1,10 +1,10 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
-def hello(update: Update) -> None:
+def hello(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Привет, хотите выгодно обменять валюту?{update.effective_user.first_name}')
     
-updater = Updater('5717266163:AAHZrdJfDETJCYjuvMg8F79q9TSK6ihcVK4')
+updater = Updater('5717266163:AAHZrdJfDETJCYjuvMg8F79q9TSK6ihcVK4', use_context=True)
 updater.dispatcher.add_handler(CommandHandler('Hello', hello))
 updater.start_polling()
 updater.idle()
