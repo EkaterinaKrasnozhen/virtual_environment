0e683bf6e819b34246aea8f3a87051cb80c7dad1
kate <01redcat@mail.ru>
2022-11-03 12:25:56 +0300

    bot chenge to do

--- venv/bot.py
+++ venv/bot.py
@@ -3,7 +3,7 @@ bot = telebot.TeleBot('5483567139:AAFOLwOJcEr_oN7xhAz82MybfR-w4RIJ--A')
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == '/start':
-       print('Привет, хотите совершить выгодный обмен?\n 1 - Купить usd за rub\n 2 - Продать usd за rub')
+       bot.send_message(message.from_user.id,'Привет, хотите совершить выгодный обмен?\n 1 - Купить usd за rub\n 2 - Продать usd за rub')
        if message.text == '1':
           bot.send_message(message.from_user.id,'Курс на сегодняшний день 1 USD = 62 RUB')
        elif message.text == '2':
