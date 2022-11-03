d226894ac22cfa02de5cfe1e6d454c6609097cad
kate <01redcat@mail.ru>
2022-11-03 12:43:03 +0300

    bot

--- venv/bot.py
+++ venv/bot.py
@@ -4,7 +4,8 @@ bot = telebot.TeleBot('5483567139:AAFOLwOJcEr_oN7xhAz82MybfR-w4RIJ--A')
 def get_text_messages(message):
     if message.text == '/start':
        bot.send_message(message.from_user.id,'Привет, хотите совершить выгодный обмен?\n 1 - Купить usd за rub\n 2 - Продать usd за rub')
-      
+       get_new_mes(message)
+
 def get_new_mes(mes):
     if mes.text == '1':
            bot.send_message(mes.from_user.id,'Курс на сегодняшний день 1 USD = 62 RUB')
