abbaeefe402e52b046a68360acae9c16db7d771b
kate <01redcat@mail.ru>
2022-11-03 12:31:34 +0300

    bot todo

--- venv/bot.py
+++ venv/bot.py
@@ -4,11 +4,11 @@ bot = telebot.TeleBot('5483567139:AAFOLwOJcEr_oN7xhAz82MybfR-w4RIJ--A')
 def get_text_messages(message):
     if message.text == '/start':
        bot.send_message(message.from_user.id,'Привет, хотите совершить выгодный обмен?\n 1 - Купить usd за rub\n 2 - Продать usd за rub')
-    if message.text == '1':
+       if message.text == '1':
            bot.send_message(message.from_user.id,'Курс на сегодняшний день 1 USD = 62 RUB')
-    elif message.text == '2':
+       elif message.text == '2':
           bot.send_message(message.from_user.id, 'Курс на сегодняшний день 1 USD = 60 RUB')
-    else:
+       else:
            bot.send_message(message.from_user.id, 'Я тебя не понимаю')
         
 bot.polling(none_stop=True, interval=0)
