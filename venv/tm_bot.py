74afcf1e95aa2fd94bf564cc0a8eb8aec463c5fe
kate <01redcat@mail.ru>
2022-11-03 14:24:08 +0300

    1

--- venv/tm_bot.py
+++ venv/tm_bot.py
@@ -7,7 +7,7 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-app = ApplicationBuilder().token("5717266163:AAHZrdJfDETJCYjuvMg8F79q9TSK6ihcVK4").build()
+app = ApplicationBuilder().token("5601693845:AAFWtvMlyKRf3v2pAG-SS9ItcI-oWwaen5A").build()
 
 app.add_handler(CommandHandler("hello", hello))
 
