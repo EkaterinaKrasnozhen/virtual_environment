1de1e020ba865d36a28e13bb71f58584fd45a2b9
kate <01redcat@mail.ru>
2022-11-04 16:56:36 +0300

    bot

--- seminar9.py/bot.py
+++ seminar9.py/bot.py
@@ -4,9 +4,28 @@ bot = telebot.TeleBot('5483567139:AAFOLwOJcEr_oN7xhAz82MybfR-w4RIJ--A')
 def get_text_messages(message):
     if message.text == '/start':
        bot.send_message(message.from_user.id,'Привет, хотите совершить выгодный обмен?\n 1 - Купить usd за rub\n 2 - Продать usd за rub')
-       
+       bot.register_next_step_handler()
 
+course_usd_rub = 60
+course_rub_usd = 62
 
+@bot.message_handler(content_types=['text'])
+def get_course_usd(message):
+    if message.text == 'u':
+        bot.send_message('Какой сегодня курс обмена usd на rub?')
+        bot.register_next_step_handler(message,set_course_usd)
 
+def set_course_usd(message):
+    course_usd_rub = message.text
+    return course_usd_rub
         
+def get_course_rub(message):
+    if message.text == 'r':
+        bot.send_message('Какой сегодня курс покупки usd за rub?')
+        bot.register_next_step_handler(message,set_course_rub)
+        
+def set_course_rub(message):
+    course_rub_usd = message.text
+    return course_rub_usd
+
 bot.polling(none_stop=True, interval=0)
