4cdcd24648450061da05ebc7fb4cf161a6f0cf75
kate <01redcat@mail.ru>
2022-11-04 16:58:04 +0300

    bot

--- seminar9.py/bot.py
+++ seminar9.py/bot.py
@@ -4,7 +4,7 @@ bot = telebot.TeleBot('5483567139:AAFOLwOJcEr_oN7xhAz82MybfR-w4RIJ--A')
 def get_text_messages(message):
     if message.text == '/start':
        bot.send_message(message.from_user.id,'Привет, хотите совершить выгодный обмен?\n 1 - Купить usd за rub\n 2 - Продать usd за rub')
-       bot.register_next_step_handler()
+       bot.register_next_step_handler(message, get_course_usd)
 
 course_usd_rub = 60
 course_rub_usd = 62
