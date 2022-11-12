b59a411e48c0c637adcb1e1f2c384f168f7bbf2c
kate <01redcat@mail.ru>
2022-11-12 19:28:05 +0300

    change_us bot to do

--- seminar9.py/bot.py
+++ seminar9.py/bot.py
@@ -2,11 +2,11 @@ import telebot
 from telebot import types
 import datetime
 import time
-     
-        
+
+
 bot = telebot.TeleBot('5483567139:AAFOLwOJcEr_oN7xhAz82MybfR-w4RIJ--A')
 
-keyboard1 = telebot.types.ReplyKeyboardMarkup(True,True)
+keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
 keyboard1.row('Купить USD за RUB', 'Продать USD за RUB')
 
 
@@ -16,46 +16,18 @@ def start_message(message):
                      \n Для выхода в главное меню - нажми \n/start""", reply_markup=keyboard1)
 
 
-# @bot.message_handler(commands=['u'])
-# def get_us(message):
-#     msg = bot.send_message(message.chat.id, 'Привет, какой сегодня курс USD/RUB?')
-#     bot.register_next_step_handler(msg, log_course_us)
-    
-
-# @bot.message_handler(commands=['r'])
-# def get_rub(message):
-#     msg1 = bot.send_message(message.chat.id, 'Привет, какой сегодня курс RUB/USD?')
-#     bot.register_next_step_handler(msg1, log_course_rub)
-    
-
-# def log_course_us(message):
-#     course_us_rub = {}
-#     to_day = f'{datetime.datetime.now():%d/%m/%y %H.%M.%S}'
-#     course_us_rub[to_day] = {message.text}
-#     with open('cours_us.cvs', 'a', encoding='utf-8') as c:
-#         c.write(f'{course_us_rub}\n')
-#     with open('cours_today_us.cvs', 'w', encoding='utf-8') as c:
-#         c.write(f'{course_us_rub}\n')
-        
-        
-# def log_course_rub(message):
-#     course_rub_us = {}
-#     to_day = f'{datetime.datetime.now():%d/%m/%y %H.%M.%S}'
-#     course_rub_us[to_day] = {message.text}
-#     with open('cours_rub.cvs', 'a', encoding='utf-8') as c:
-#         c.write(f'{course_rub_us}\n')
-#     with open('cours_today_rub.cvs', 'w', encoding='utf-8') as c:
-#         c.write(f'{course_rub_us}\n')
-        
 @bot.message_handler(commands=['r'])
 def get_rub_1(message):
-    msg1 = bot.send_message(message.chat.id, 'Привет, какой сегодня курс продажи USD за RUB?')
+    msg1 = bot.send_message(
+        message.chat.id, 'Привет, какой сегодня курс продажи USD за RUB?')
     id_1 = 1
     bot.register_next_step_handler(msg1, log_course, id_1)
 
+
 @bot.message_handler(commands=['u'])
 def get_us_1(message):
-    msg = bot.send_message(message.chat.id, 'Привет, какой сегодня курс покупки USD за RUB?')
+    msg = bot.send_message(
+        message.chat.id, 'Привет, какой сегодня курс покупки USD за RUB?')
     id_2 = 2
     bot.register_next_step_handler(msg, log_course, id_2)
 
@@ -74,25 +46,13 @@ def log_course(message, num):
             c.write(f'{course}\n')
         with open('cours_today_rub.cvs', 'w', encoding='utf-8') as c:
             c.write(f'{course}\n')
-        
+
 
 def get_course_today_us():
     dict1 = dict()
     data = open('cours_today_us.cvs', 'r', encoding='utf-8')
-    file = data.read().replace("{","").replace("}","").replace("'","").split("\n")[:-1]
-    data.close
-    for item in file:
-        key = item.split(':')[0]
-        value = item.split(':')[1]
-        dict1[key] = value
-        data.close()
-        return value
-    
-    
-def get_course_today_rub():
-    dict1 = dict()
-    data = open('cours_today_rub.cvs', 'r', encoding='utf-8')
-    file = data.read().replace("{","").replace("}","").replace("'","").split("\n")[:-1]
+    file = data.read().replace("{", "").replace(
+        "}", "").replace("'", "").split("\n")[:-1]
     data.close
     for item in file:
         key = item.split(':')[0]
@@ -101,18 +61,72 @@ def get_course_today_rub():
         data.close()
         return value
 
+
+def get_course_today_rub():
+    dict1 = dict()
+    data = open('cours_today_rub.cvs', 'r', encoding='utf-8')
+    file = data.read().replace("{", "").replace(
+        "}", "").replace("'", "").split("\n")[:-1]
+    data.close
+    for item in file:
+        key = item.split(':')[0]
+        value = item.split(':')[1]
+        dict1[key] = value
+        data.close()
+        return value
+    
+    
+# def get_course_today(id_n):
+#     dict1 = dict()
+#     if id_n == 1:
+#         data = open('cours_today_rub.cvs', 'r', encoding='utf-8')
+#         file = data.read().replace("{", "").replace("}", "").replace("'", "").split("\n")[:-1]
+#         data.close
+#         for item in file:
+#              key = item.split(':')[0]
+#              value = item.split(':')[1]
+#              dict1[key] = value
+#              data.close()
+#              return value
+#     elif id_n == 2:
+#         data = open('cours_today_us.cvs', 'r', encoding='utf-8')
+#         file = data.read().replace("{", "").replace("}", "").replace("'", "").split("\n")[:-1]
+#         data.close
+#         for item in file:
+#              key = item.split(':')[0]
+#              value = item.split(':')[1]
+#              dict1[key] = value
+#              data.close()
+#              return value
+        
+# @bot.message_handler(content_types=['text'])
+# def message_reply_1(message):
+#     if message.text == 'Купить USD за RUB':
+#         bot.send_message(
+#             message.chat.id, f'курс сегодня = {get_course_today_us()}')
+#         return
+#     if message.text == 'Продать USD за RUB':
+#         bot.send_message(
+#             message.chat.id, f'курс сегодня = {get_course_today_rub()}')
+#         return
+#     else:
+#         bot.send_message(
+#             message.chat.id, 'Я тебя не понимаю, нажми /start и выбери нужную кнопку меню.')
 
 @bot.message_handler(content_types=['text'])
 def message_reply(message):
     if message.text == 'Купить USD за RUB':
-        bot.send_message(message.chat.id, f'курс сегодня = {get_course_today_us()}')
+        bot.send_message(
+            message.chat.id, f'курс сегодня = {get_course_today_us()}')
         return
     if message.text == 'Продать USD за RUB':
-        bot.send_message(message.chat.id, f'курс сегодня = {get_course_today_rub()}')
+        bot.send_message(
+            message.chat.id, f'курс сегодня = {get_course_today_rub()}')
         return
     else:
-        bot.send_message(message.chat.id, 'Я тебя не понимаю, нажми /start и выбери нужную кнопку меню.')
- 
+        bot.send_message(
+            message.chat.id, 'Я тебя не понимаю, нажми /start и выбери нужную кнопку меню.')
+
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
@@ -130,4 +144,3 @@ def sticker_id(message):
 
 
 bot.polling(none_stop=True, interval=0)
-
