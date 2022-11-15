11ad22fc130f9e410c703afe187aa82d0f703f47
kate <01redcat@mail.ru>
2022-11-15 13:44:37 +0300

    h/w формула с мат анализом

---
+++ seminar11/name_main.py
@@ -0,0 +1,8 @@
+def main():#если без функии код написан и в др.файле import name_main сделать, то выполнится весь код написанный
+    print('file1')
+    x = 5
+    return x
+
+
+if __name__ == '__main__':
+    main()
---
+++ seminar11/name_main2.py
@@ -0,0 +1,4 @@
+import name_main
+
+print('file2')
+print(name_main.main())
